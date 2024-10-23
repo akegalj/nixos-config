0225922c91639974ab5b5628822ebec585d89276
Ante Kegalj <akegalj@gmail.com>
2024-10-23 08:35:53 +0200

    Remove comment

--- home.nix
+++ home.nix
@@ -114,8 +114,7 @@ in
   programs = {
     gnupg.agent.enable = true;
     # gnupg.agent.enableSSHSupport = true;
-    # Use ssh-add to add a key to the agent
-    # FIXME: agent is not active yet (more work has to be done here)
+    # NOTE: Use ssh-add to add a key to the agent
     ssh.startAgent = true;
     git = {
       enable = true;
